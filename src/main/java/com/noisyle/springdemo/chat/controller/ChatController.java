89789b94cfe52310a4a7b1281fa64eab9e6d572a
noisyle <anyone.1984@gmail.com>
2015-05-14 17:00:31 +0800

    优化

--- src/main/java/com/noisyle/springdemo/chat/controller/ChatController.java
+++ src/main/java/com/noisyle/springdemo/chat/controller/ChatController.java
@@ -1,5 +1,11 @@
 package com.noisyle.springdemo.chat.controller;
 
+import java.text.DecimalFormat;
+import java.util.Date;
+import java.util.Random;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ChatController {
 	@RequestMapping("/chat")
-	public String index(Model model) {
+	public String index(HttpServletRequest request, Model model) {
+		String username = (String) request.getSession().getAttribute("username");
+		if(username==null){
+			Random r = new Random(new Date().getTime());
+			DecimalFormat df = new DecimalFormat("000000");
+			username = "匿名用户"+df.format(r.nextInt(999999));
+			request.getSession().setAttribute("username", username);
+		}
+		model.addAttribute("name", username); 
 		return "chat/index";
 	}
 	
